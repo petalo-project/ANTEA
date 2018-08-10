a73184073a641a7409a17a7e46750bc3c819c37d
Paola Ferrario <paola@nextp07.ific.uv.es>
2018-08-10 18:54:20 +0200

    Change the name of the variable to a more appropriate one.

--- antea/utils/table_functions.py
+++ antea/utils/table_functions.py
@@ -5,4 +5,4 @@ from invisible_cities.io.dst_io   import load_dst
 def load_rpos(filename, group = "Radius",
                         node  = "f100bins"):
     dst = load_dst(filename, group, node)
-    return Correction((dst.Ratio.values,), dst.Rpos.values, dst.Uncertainty.values)
+    return Correction((dst.RmsPhi.values,), dst.Rpos.values, dst.Uncertainty.values)
