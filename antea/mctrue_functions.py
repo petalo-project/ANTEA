c4e1728e59b5f027a45ed3ddfc6095c915c6fab5
Paola Ferrario <paola.ferrario@gmail.com>
2019-07-10 10:14:52 +0200

    Add reconstruction and true MC functions + script to build R map.

---
+++ antea/mctrue_functions.py
@@ -0,0 +1,50 @@
+import numpy  as np
+import pandas as pd
+
+import antea.reco.reco_functions as rf
+
+from typing import Sequence, Tuple
+
+
+def find_hits_of_given_particles(p_ids: Sequence[int], hits: pd.DataFrame) -> pd.DataFrame:
+    """
+    Select only the hits belonging to given particles.
+    """
+    return hits[hits.particle_id.isin(p_ids)]
+
+def select_photoelectric(evt_parts: pd.DataFrame, evt_hits: pd.DataFrame) -> bool:
+    """
+    Select only the events where one or two photoelectric events occur, and nothing else.
+    """
+    sel_volume   = (evt_parts.initial_volume == 'ACTIVE') & (evt_parts.final_volume == 'ACTIVE')
+    sel_name     =  evt_parts.name == 'e-'
+    sel_vol_name = evt_parts[sel_volume & sel_name]
+    ids          = sel_vol_name.particle_id.values
+
+    sel_hits   = mcf.find_hits_of_given_particles(ids, evt_hits)
+    energies   = sel_hits.groupby(['particle_id'])[['energy']].sum()
+    energies   = energies.reset_index()
+    energy_sel = energies[rf.greater_or_equal(energies.energy, 0.476443, allowed_error=1.e-6)]
+
+    sel_vol_name_e  = sel_vol_name[sel_vol_name.particle_id.isin(energy_sel.particle_id)]
+
+    primaries = evt_parts[evt_parts.primary == True]
+    sel_all   = sel_vol_name_e[sel_vol_name_e.mother_id.isin(primaries.particle_id.values)]
+    if len(sel_all) == 0:
+        return False
+
+    ### Once the event has passed the selection, let's calculate the true position(s)
+    ids      = sel_all.particle_id.values
+    sel_hits = mcf.find_hits_of_given_particles(ids, evt_hits)
+
+    sel_hits = sel_hits.groupby(['particle_id'])
+    true_pos = []
+    for _, df in sel_hits:
+        hit_positions = np.array([df.x, df.y, df.z]).transpose()
+        true_pos.append(np.average(hit_positions, axis=0, weights=df.energy))
+
+    ### Reject events where the two gammas have interacted in the same emisphere.
+    if (len(true_pos) == 1) & (evt_hits.energy.sum() > 0.513):
+       return False
+
+   return True
---
+++ antea/reco/reco_functions_test.py
@@ -0,0 +1,34 @@
+import numpy  as np
+import pandas as pd
+
+from hypothesis.strategies import floats
+from hypothesis            import given
+
+from . reco_functions import lower_or_equal, greater_or_equal
+
+
+f             = floats(min_value=1, max_value=2)
+f_lower       = floats(min_value=0, max_value=1)
+allowed_error = floats(min_value=1.e-8, max_value=1.e-6)
+
+
+@given(f, f_lower)
+def test_lower_or_equal(f1, f2):
+   assert lower_or_equal(f2, f1)
+
+   
+@given(f, f_lower)
+def test_greater_or_equal(f1, f2):
+   assert greater_or_equal(f1, f2)
+
+
+@given(f, allowed_error)
+def test_allowed_error_in_inequality(f1, err):
+    f2 = f1 + err
+    assert greater_or_equal(f1, f2, err)
+
+    f2 = f1 - err
+    assert lower_or_equal(f1, f2, err)
+
+    f2 = f1 + 2*err
+    assert not greater_or_equal(f1, f2, err)
