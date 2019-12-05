aaccbd34c906779a2b066b12d92067dd8df33027
Carmen Romo <carmenromoluque@gmail.com>
2019-12-05 20:05:53 +0100

    Include daq functions for tof

--- antea/elec/tof_functions.py
+++ antea/elec/tof_functions.py
@@ -1,2 +1,57 @@
 import numpy  as np
 import pandas as pd
+
+
+def spe_dist(tau_sipm: Tuple[float, float], time: Sequence[float]) -> Sequence[float]:
+    """
+    Double exponential decay for the sipm response.
+    """
+    alfa         = 1.0/tau_sipm[1]
+    beta         = 1.0/tau_sipm[0]
+    t_p          = np.log(beta/alfa)/(beta-alfa)
+    K            = (beta)*np.exp(alfa*t_p)/(beta-alfa)
+    spe_response = K*(np.exp(-time*alfa)-np.exp(-time*beta))
+    return spe_response
+
+
+def convolve_tof(spe_response: Sequence[float], signal: Sequence[float]) -> Sequence[float]:
+    """
+    Apply the spe_response distribution to the given signal.
+    """
+    conv_first = np.hstack([spe_response, np.zeros(len(signal)-1)])
+    conv_res   = np.zeros(len(signal)+len(spe_response)-1)
+    pe_pos     = np.argwhere(signal > 0)
+    pe_recov   = signal[pe_pos]
+    for i in range(len(pe_recov)): #Loop over the charges
+        desp     = np.roll(conv_first, pe_pos[i])*pe_recov[i]
+        conv_res += desp
+    return conv_res
+
+
+def TDC_convolution(tof_response: pd.DataFrame, spe_response: Sequence[float], time_window: float, n_sipms: int, first_sipm: int, te_tdc: float) -> Sequence[float]:
+    """
+    Apply the spe_response distribution to every sipm and returns an array
+    with the first timestamp for every sensor.
+    """
+    pe_table  = np.zeros((time_window, n_sipms))
+    for i, wf in tof_response.iterrows():
+        if wf.time_bin < time_window:
+            s_id = - wf.sensor_id - first_sipm
+            pe_table[wf.time_bin, s_id] = wf.charge
+
+    conv_table = np.zeros((len(pe_table) + len(spe_response)-1, n_sipms))
+    for i in range(n_sipms):
+        if np.count_nonzero(pe_table[:,i]):
+            conv_table[:,i] = convolve_tof(spe_response, pe_table[0:time_window,i])
+
+    timestamp_v = []
+    for i in range(n_sipms):
+        timestamp  = np.argwhere(conv_table[0:time_window,i] > te_tdc)
+        # We take only the first part up to time_window to speed up the computation
+        if len(timestamp)==0:
+            timestamp = 0
+        else:
+            timestamp = np.min(timestamp)
+
+        timestamp_v.append(timestamp) ## List with the first time for each sensor
+    return np.array(timestamp_v)
