da9115ee06e24fd78aabfdf1425aecb45614184e
Carmen Romo <carmenromoluque@gmail.com>
2019-12-17 13:26:35 +0100

    Add test_convolve_tof

--- antea/elec/tof_functions_test.py
+++ antea/elec/tof_functions_test.py
@@ -23,3 +23,17 @@ def test_spe_dist(a, b, l):
         assert len(exp_dist) == len(np.unique(l))
         assert (exp_dist >= 0.).all()
         assert np.isclose(np.sum(exp_dist), 1)
+
+
+t = st.tuples(st.floats(min_value=1, max_value=1000), st.floats(min_value=2, max_value=1000))
+s = st.lists(st.integers(min_value=1, max_value=10000), min_size=2, max_size=1000)
+
+@given(t, l, s)
+def test_convolve_tof(t, l, s):
+    spe_response = tf.spe_dist(t, np.unique(l))
+    conv_res     = tf.convolve_tof(spe_response, np.array(s))
+    assert len(conv_res) == len(spe_response) + len(s) - 1
+    if np.isclose(1/t[0], 1/t[1], atol=1e-2):
+        assert np.sum(conv_res) == 0
+    else:
+        assert np.isclose(np.sum(s), np.sum(conv_res))
