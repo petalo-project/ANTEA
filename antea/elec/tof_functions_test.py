2eeda7e7ebaf8f59be86b548dc0a9934b7ad33fb
Carmen Romo <carmenromoluque@gmail.com>
2020-01-17 13:32:30 +0100

    Add test_tdc_convolution

--- antea/elec/tof_functions_test.py
+++ antea/elec/tof_functions_test.py
@@ -37,3 +37,26 @@ def test_convolve_tof(l, s):
         assert np.isclose(np.sum(s), np.sum(conv_res))
 
 
+def test_tdc_convolution(ANTEADATADIR):
+    """
+    Check that the function tdc_convolution returns a table with the adequate dimentions and in case the tof dataframe is empty, checks that the table only contains zeros.
+    """
+    PATH_IN        = os.path.join(ANTEADATADIR, 'ring_test_1000ev.h5')
+    tof_response   = load_mcTOFsns_response(PATH_IN)
+    SIPM           = {'n_sipms':3500, 'first_sipm':1000, 'tau_sipm':[100,15000]}
+    n_sipms        = SIPM['n_sipms']
+    first_sipm     = SIPM['first_sipm']
+    tau_sipm       = SIPM['tau_sipm']
+    TE_range       = [0.25]
+    TE_TDC         = TE_range[0]
+    time_window    = 10000
+    time_bin       = 5
+    time           = np.arange(0, 80000, time_bin)
+    spe_resp, norm = tf.spe_dist(tau_sipm, time)
+    tdc_conv_table = tf.tdc_convolution(tof_response, spe_resp, time_window, n_sipms, first_sipm, TE_TDC)
+    assert tdc_conv_table.shape == (time_window + len(spe_resp)-1, n_sipms)
+
+    keys                 = np.array(['event_id', 'sensor_id', 'time_bin', 'charge'])
+    wf_df                = pd.DataFrame({}, columns=keys)
+    tdc_conv_table_zeros = tf.tdc_convolution(wf_df, spe_resp, time_window, n_sipms, first_sipm, TE_TDC)
+    assert np.all(tdc_conv_table_zeros==0)
