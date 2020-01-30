cab26c164a5205d762e6d350e3c3403874f06559
Paola Ferrario <paola.ferrario@gmail.com>
2020-01-30 13:39:32 +0100

    Add functions for 3d images + change the definition for cold spheres.

--- antea/image/fom_functions.py
+++ antea/image/fom_functions.py
@@ -8,10 +8,10 @@ a background.
 
 
 def average_in_sphere2d(img2d : np.ndarray,
-                      sphere_r : float, r : float, phi : float,
-                      x_size : float, y_size : float, xbins : int, ybins : int) -> float:
+                        sphere_r : float, r : float, phi : float,
+                        x_size : float, y_size : float, xbins : int, ybins : int) -> float:
     """
-    Calculates the average density in a sphere of radius sphere_r, centred in (r, phi).
+    Calculates the average density in a circle of radius sphere_r, centred in (r, phi).
     x_size, y_size: size of the image (in length unit).
     xbins, ybins: number of bins  in x and y.
     """
@@ -21,31 +21,69 @@ def average_in_sphere2d(img2d : np.ndarray,
     cx = r*np.cos(phi)
     cy = r*np.sin(phi)
 
-    sphere_bins = []
-    for i in range(xbins):
-        for j in range(ybins):
-            x = -x_size/2 + i*x_bin_width + x_bin_width/2.
-            y = -y_size/2 + j*y_bin_width + y_bin_width/2.
+    i = np.array(range(xbins))
+    j = np.array(range(ybins))
 
-            if np.sqrt((x-cx)**2 + (y-cy)**2) < sphere_r:
-                sphere_bins.append(img2d[i, j])
+    x = -x_size/2 + i*x_bin_width + x_bin_width/2.
+    y = -y_size/2 + j*y_bin_width + y_bin_width/2.
 
-    return np.average(sphere_bins)
+    comb_xy = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+    comb_ij = np.array(np.meshgrid(i, j)).T.reshape(-1, 2)
+
+    in_sphere = np.sqrt((comb_xy[:, 0]-cx)**2 + (comb_xy[:, 1]-cy)**2) < sphere_r
+    img_bins  = comb_ij[in_sphere]
+
+    return np.average(img2d[img_bins[:, 0], img_bins[:, 1]])
+
+
+def average_in_sphere3d(img3d : np.ndarray,
+                        sphere_r : float, r : float, phi : float,
+                        x_size : float, y_size : float, z_size : float,
+                        xbins : int, ybins : int, zbins : int) -> float:
+    """
+    Calculates the average density in a sphere of radius sphere_r, centred in (r, phi).
+    x_size, y_size, z_size: size of the image (in length unit).
+    xbins, ybins, zbins: number of bins in x, y and z.
+    """
+
+    x_bin_width = x_size / xbins
+    y_bin_width = y_size / ybins
+    z_bin_width = z_size / zbins
+    cx = r*np.cos(phi)
+    cy = r*np.sin(phi)
+    cz = 0.
+
+    i = np.array(range(xbins))
+    j = np.array(range(ybins))
+    k = np.array(range(zbins))
+
+    x = -x_size/2 + i*x_bin_width + x_bin_width/2.
+    y = -y_size/2 + j*y_bin_width + y_bin_width/2.
+    z = -z_size/2 + k*z_bin_width + z_bin_width/2.
+
+    comb_xyz = np.array(np.meshgrid(x, y, z)).T.reshape(-1, 3)
+    comb_ijk = np.array(np.meshgrid(i, j, k)).T.reshape(-1, 3)
+
+    in_sphere = np.sqrt((comb_xyz[:, 0] - cx)**2 + (comb_xyz[:, 1] - cy)**2 +
+                        (comb_xyz[:, 2] - cz)**2) < sphere_r
+    img_bins  = comb_ijk[in_sphere]
+
+    return np.average(img3d[img_bins[:, 0], img_bins[:, 1], img_bins[:, 2]])
 
 
 def average_in_bckg2d(img2d : np.ndarray,
-                    sphere_r : float, r : float,
-                    phi_start : float, phi_step : float, n_phi : int,
-                    x_size : float, y_size : float, xbins : int, ybins: int) -> float:
+                      sphere_r : float, r : float,
+                      phi_start : float, phi_step : float, n_phi : int,
+                      x_size : float, y_size : float, xbins : int, ybins: int) -> float:
     """
-    Calculates the average background density taking the average of n_phi spheres
+    Calculates the average background density taking the average of n_phi circles
     of radius sphere_r, centred at radius r and phi == phi_start + n*phi_step.
     """
 
     bckg_ave = 0
     for i in range(n_phi):
         bckg = average_in_sphere2d(img2d, sphere_r, r, phi_start + i*phi_step, x_size,
-                                 y_size, xbins, ybins)
+                                   y_size, xbins, ybins)
         bckg_ave += bckg
 
     bckg_ave /= n_phi
@@ -53,10 +91,31 @@ def average_in_bckg2d(img2d : np.ndarray,
     return bckg_ave
 
 
-def crc2d(img2d : np.ndarray, max_intensity : int,
-        sig_sphere_r : float, r : float, phi : float,
-        bckg_sphere_r : float, phi0 : float, phi_step : float, nphi : int,
-        x_size : float, y_size : float, xbins : int, ybins : int) -> float:
+def average_in_bckg3d(img3d : np.ndarray,
+                      sphere_r : float, r : float,
+                      phi_start : float, phi_step : float, n_phi : int,
+                      x_size : float, y_size : float, z_size : float,
+                      xbins : int, ybins: int, zbins : int) -> float:
+    """
+    Calculates the average background density taking the average of n_phi spheres
+    of radius sphere_r, centred at radius r and phi == phi_start + n*phi_step.
+    """
+
+    bckg_ave = 0
+    for i in range(n_phi):
+        bckg = average_in_sphere3d(img3d, sphere_r, r, phi_start + i*phi_step, x_size,
+                                   y_size, z_size, xbins, ybins, zbins)
+        bckg_ave += bckg
+
+    bckg_ave /= n_phi
+
+    return bckg_ave
+
+
+def crc_hot2d(img2d : np.ndarray, max_intensity : int,
+              sig_sphere_r : float, r : float, phi : float,
+              bckg_sphere_r : float, phi0 : float, phi_step : float, nphi : int,
+              x_size : float, y_size : float, xbins : int, ybins : int) -> float:
     """
     Calculates the contrast_recovery_coefficent of an image.
     img2d: 2D np.array with the image.
@@ -74,6 +133,85 @@ def crc2d(img2d : np.ndarray, max_intensity : int,
 
     signal = average_in_sphere2d(img2d, sig_sphere_r, r, phi, x_size, y_size, xbins, ybins)
     bckg   = average_in_bckg2d(img2d, bckg_sphere_r, r, phi0, phi_step, nphi,
-                             x_size, y_size,  xbins, ybins)
+                               x_size, y_size,  xbins, ybins)
 
     return signal / bckg / max_intensity
+
+
+def crc_hot3d(img3d : np.ndarray, max_intensity : int,
+              sig_sphere_r : float, r : float, phi : float,
+              bckg_sphere_r : float, phi0 : float, phi_step : float, nphi : int,
+              x_size : float, y_size : float, z_size : float,
+              xbins : int, ybins : int, zbins : int):
+    """
+    Calculates the contrast_recovery_coefficent of an image.
+    img2d: 2D np.array with the image.
+    max_intensity: maximum value of original image pixels.
+    sig_sphere_r: radius of the sphere of signal.
+    r: radial position of both signal and background spheres.
+    phi: angular position of signal sphere.
+    bckg_sphere_r: radius of the sphere of background.
+    phi0: angular position of first background sphere.
+    phi_step: angular step of background spheres.
+    nphi: number of backgrounds spheres used for average.
+    x_size, y_size, z_size: size of the image in length unit.
+    xbins, ybins, zbins: number of bins of the image.
+    """
+
+    signal = average_in_sphere3d(img3d, sig_sphere_r, r, phi, x_size, y_size, z_size, xbins, ybins, zbins)
+    bckg   = average_in_bckg3d(img3d, bckg_sphere_r, r, phi0, phi_step, nphi,
+                               x_size, y_size, z_size, xbins, ybins, zbins)
+
+    return signal / bckg / max_intensity
+
+
+def crc_cold2d(img2d : np.ndarray, sig_sphere_r : float, r : float, phi : float,
+               bckg_sphere_r : float, phi0 : float, phi_step : float, nphi : int,
+               x_size : float, y_size : float, xbins : int, ybins : int) -> float:
+    """
+    Calculates the contrast_recovery_coefficent of an image.
+    img2d: 2D np.array with the image.
+    max_intensity: maximum value of original image pixels.
+    sig_sphere_r: radius of the sphere of signal.
+    r: radial position of both signal and background spheres.
+    phi: angular position of signal sphere.
+    bckg_sphere_r: radius of the sphere of background.
+    phi0: angular position of first background sphere.
+    phi_step: angular step of background spheres.
+    nphi: number of backgrounds spheres used for average.
+    x_size, y_size: size of the image in length unit.
+    xbins, ybins: number of bins of the image.
+    """
+
+    signal = average_in_sphere2d(img2d, sig_sphere_r, r, phi, x_size, y_size, xbins, ybins)
+    bckg   = average_in_bckg2d(img2d, bckg_sphere_r, r, phi0, phi_step, nphi,
+                               x_size, y_size,  xbins, ybins)
+
+    return 1 - signal / bckg
+
+
+def crc_cold3d(img3d : np.ndarray, sig_sphere_r : float, r : float, phi : float,
+               bckg_sphere_r : float, phi0 : float, phi_step : float, nphi : int,
+               x_size : float, y_size : float, z_size : float,
+               xbins : int, ybins : int, zbins : int) -> float:
+    """
+    Calculates the contrast_recovery_coefficent of an image.
+    img2d: 2D np.array with the image.
+    max_intensity: maximum value of original image pixels.
+    sig_sphere_r: radius of the sphere of signal.
+    r: radial position of both signal and background spheres.
+    phi: angular position of signal sphere.
+    bckg_sphere_r: radius of the sphere of background.
+    phi0: angular position of first background sphere.
+    phi_step: angular step of background spheres.
+    nphi: number of backgrounds spheres used for average.
+    x_size, y_size: size of the image in length unit.
+    xbins, ybins: number of bins of the image.
+    """
+
+    signal = average_in_sphere3d(img3d, sig_sphere_r, r, phi, x_size, y_size, z_size,
+                                 xbins, ybins, zbins)
+    bckg   = average_in_bckg3d(img3d, bckg_sphere_r, r, phi0, phi_step, nphi,
+                               x_size, y_size, z_size, xbins, ybins, zbins)
+
+    return 1 - signal / bckg
--- antea/image/fom_functions_test.py
+++ antea/image/fom_functions_test.py
@@ -23,36 +23,55 @@ phi_step      = np.pi/3.
 nphi          = 6
 x_size        = 180
 y_size        = 180
+z_size        = 180
 xbins         = 180
 ybins         = 180
+zbins         = 180
 
 hot_sphere_r = [4, 6.5, 8.5, 11]
 hot_phi      = [np.pi/3., 2.*np.pi/3., 3.*np.pi/3., 4*np.pi/3.]
-cold_sphere_r = [14, 18.5, 22.]
-cold_phi      = [5*np.pi/3.,  6*np.pi/3., 0.]
+cold_sphere_r = [14, 18.5]
+cold_phi      = [5*np.pi/3., 6*np.pi/3.]
 
 
 def test_true_signal_crc_is_close_to_one(phantom_true_img):
 
-    ### take one bin in z, in the centre  of the image
+    ### take one bin in z, in the centre of the image
     img_slice = np.sum(phantom_true_img[:,:,89:90], axis=2)
 
     for i in range(0, len(hot_phi)):
-        crc = fomf.crc2d(img_slice, max_intensity, hot_sphere_r[i], r, hot_phi[i],
-                        bckg_sphere_r, phi0, phi_step, nphi, x_size, y_size,
-                        xbins, ybins)
+        crc = fomf.crc_hot2d(img_slice, max_intensity, hot_sphere_r[i],
+                             r, hot_phi[i], bckg_sphere_r, phi0, phi_step, nphi,
+                             x_size, y_size, xbins, ybins)
+
+        assert np.isclose(crc, 1, rtol=5e-02, atol=5e-02)
+
+    ### take the 3d image
+    for i in range(0, len(hot_phi)):
+        crc = fomf.crc_hot3d(phantom_true_img, max_intensity, hot_sphere_r[i],
+                             r, hot_phi[i], bckg_sphere_r, phi0, phi_step, nphi,
+                             x_size, y_size, z_size,
+                             xbins, ybins, zbins)
 
         assert np.isclose(crc, 1, rtol=5e-02, atol=5e-02)
 
 
 def test_true_background_crc_is_close_to_zero(phantom_true_img):
 
-    ### take one bin in z, in the centre  of the image
+    ### take one bin in z, in the centre of the image
     img_slice = np.sum(phantom_true_img[:,:,89:90], axis=2)
 
     for i in range(0, len(cold_phi)):
-        crc = fomf.crc2d(img_slice, max_intensity, cold_sphere_r[i], r, cold_phi[i],
-                        bckg_sphere_r, phi0, phi_step, nphi, x_size, y_size,
-                        xbins, ybins)
+        crc = fomf.crc_cold2d(img_slice, cold_sphere_r[i],
+                              r, cold_phi[i], bckg_sphere_r, phi0, phi_step, nphi,
+                              x_size, y_size, xbins, ybins)
 
-        assert np.isclose(crc, 0, rtol=8e-02, atol=8e-02)
+        assert np.isclose(crc, 1, rtol=8e-02, atol=8e-02)
+
+    ### take the 3d image
+    for i in range(0, len(cold_phi)):
+        crc = fomf.crc_cold3d(phantom_true_img, cold_sphere_r[i],
+                              r, cold_phi[i], bckg_sphere_r, phi0, phi_step, nphi,
+                              x_size, y_size, z_size, xbins, ybins, zbins)
+
+        assert np.isclose(crc, 1, rtol=8e-02, atol=8e-02)
