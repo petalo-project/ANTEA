9c9bb9965be4698aae3ee42c5e1b8168f7163bb1
carmenromo <carmenromoluque@gmail.com>
2021-04-20 17:33:01 +0200

    Simplify code

--- antea/mcsim/fastmc3d.py
+++ antea/mcsim/fastmc3d.py
@@ -53,14 +53,7 @@ def simulate_reco_event(evt_id: int, hits: pd.DataFrame, particles: pd.DataFrame
     t2 = t2 / units.ps
 
     # Transform in cylindrical coordinates
-    cyl_pos = rf.from_cartesian_to_cyl(np.array([pos1, pos2]))
-
-    r1   = cyl_pos[0, 0]
-    phi1 = cyl_pos[0, 1]
-    z1   = cyl_pos[0, 2]
-    r2   = cyl_pos[1, 0]
-    phi2 = cyl_pos[1, 1]
-    z2   = cyl_pos[1, 2]
+    (r1, phi1, z1), (r2, phi2, z2) = rf.from_cartesian_to_cyl(np.array([pos1, pos2]))
 
     # Get all errors.
     if phot1:
@@ -87,48 +80,24 @@ def simulate_reco_event(evt_id: int, hits: pd.DataFrame, particles: pd.DataFrame
     t1_reco = t1 - et1
     t2_reco = t2 - et2
 
-    event_ids = [float(evt_id)]
-    energies  = [energy]
-
-    true_r1   = [r1]
-    true_phi1 = [phi1]
-    true_z1   = [z1]
-    true_t1   = [t1]
-    true_r2   = [r2]
-    true_phi2 = [phi2]
-    true_z2   = [z2]
-    true_t2   = [t2]
-
-    phot_like1 = [float(phot1)]
-    phot_like2 = [float(phot2)]
-
-    reco_r1   = [r1_reco]
-    reco_phi1 = [phi1_reco]
-    reco_z1   = [z1_reco]
-    reco_t1   = [t1_reco]
-    reco_r2   = [r2_reco]
-    reco_phi2 = [phi2_reco]
-    reco_z2   = [z2_reco]
-    reco_t2   = [t2_reco]
-
-    events = pd.DataFrame({'event_id':  event_ids,
-                           'true_energy': energies,
-                           'true_r1':   true_r1,
-                           'true_phi1': true_phi1,
-                           'true_z1':   true_z1,
-                           'true_t1':   true_t1,
-                           'true_r2':   true_r2,
-                           'true_phi2': true_phi2,
-                           'true_z2':   true_z2,
-                           'true_t2':   true_t2,
-                           'phot_like1':phot_like1,
-                           'phot_like2':phot_like2,
-                           'reco_r1':   reco_r1,
-                           'reco_phi1': reco_phi1,
-                           'reco_z1':   reco_z1,
-                           'reco_t1':   reco_t1,
-                           'reco_r2':   reco_r2,
-                           'reco_phi2': reco_phi2,
-                           'reco_z2':   reco_z2,
-                           'reco_t2':   reco_t2})
+    events = pd.DataFrame({'event_id':    float(evt_id),
+                           'true_energy': energy,
+                           'true_r1':     r1,
+                           'true_phi1':   phi1,
+                           'true_z1':     z1,
+                           'true_t1':     t1,
+                           'true_r2':     r2,
+                           'true_phi2':   phi2,
+                           'true_z2':     z2,
+                           'true_t2':     t2,
+                           'phot_like1':  float(phot1),
+                           'phot_like2':  float(phot2),
+                           'reco_r1':     r1_reco,
+                           'reco_phi1':   phi1_reco,
+                           'reco_z1':     z1_reco,
+                           'reco_t1':     t1_reco,
+                           'reco_r2':     r2_reco,
+                           'reco_phi2':   phi2_reco,
+                           'reco_z2':     z2_reco,
+                           'reco_t2':     t2_reco}, index=[0])
     return events
