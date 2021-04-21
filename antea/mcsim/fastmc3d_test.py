0b97424289d40ec1009f5e9445840f21564ec496
carmenromo <carmenromoluque@gmail.com>
2021-04-21 17:01:20 +0200

    Add fastmc3d_test file

---
+++ antea/mcsim/fastmc3d_test.py
@@ -0,0 +1,9 @@
+
+import os
+import pandas as pd
+
+from .. reco    import reco_functions as rf
+from .. io      import mc_io          as mcio
+from .          import fastmc3d       as fmc
+from . errmat   import errmat
+from . errmat3d import errmat3d
