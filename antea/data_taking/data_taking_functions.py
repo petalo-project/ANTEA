fb536469aba73a5bd11751fc5eabd4f337cfa040
Carmen Romo <carmenromo@lapromo.ific.uv.es>
2021-11-02 12:55:28 +0100

    Add data taking functions

---
+++ antea/data_taking/data_taking_functions.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+def compute_coincidences(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns the events in which both planes have detected charge.
+    """
+    nplanes = df.groupby(['evt_number', 'cluster'])['tofpet_id'].nunique()
+    df_idx  = df.set_index(['evt_number', 'cluster'])
+    df_coincidences = df_idx.loc[nplanes[nplanes == 2].index]
+
+    return df_coincidences
+
+
+def filter_evt_with_max_charge_at_center(df: pd.DataFrame,
+                                         det_plane: bool = True,
+                                         variable: str = 'efine_corrected',
+                                         tot_mode: bool = False) -> bool:
+    """
+    Returns True if the maximum charge of the event has been detected in one of the four central sensors of the desired plane.
+    """
+    if det_plane:
+        tofpet_id   = 0
+        central_sns = [44, 45, 54, 55]
+    else:
+        tofpet_id   = 2
+        central_sns = [122, 123, 132, 133]
+
+    df = df[df.tofpet_id == tofpet_id]
+    if len(df)==0:
+        return False
+
+    if tot_mode: # t2 - t1 instead of intg_w or efine_corrected
+        argmax = (df.t2 - df.t1).argmax()
+    else:
+        argmax = df[variable].argmax()
+
+    return df.iloc[argmax].sensor_id in central_sns
+
+
+def select_evts_with_max_charge_at_center(df: pd.DataFrame,
+                                         det_plane: bool = True,
+                                         variable: str = 'efine_corrected',
+                                         tot_mode: bool = False) -> pd.DataFrame:
+    """
+    Returns a dataframe with only the events with maximum charge at the central sensors.
+    """
+    df_filter_center = df.groupby(['evt_number', 'cluster']).filter(filter_evt_with_max_charge_at_center, dropna=True, det_plane=det_plane, variable=variable, tot_mode=tot_mode)
+    
+    return df_filter_center
