66af55d2e890b0cd9ab52483062fcca294ff0361
Paola Ferrario <paola.ferrario@gmail.com>
2021-11-17 14:16:14 +0100

    Add file with full body PET simulation.

--- antea/scripts/reconstruct_coincidences_test.py
+++ antea/scripts/reconstruct_coincidences_test.py
@@ -8,7 +8,7 @@ def test_run_script(ANTEADATADIR, config_tmpdir):
     """
     Checks that the script to reconstruct the coincidences runs.
     """
-    input_file  = os.path.join(ANTEADATADIR, 'ring_test_1000ev.h5')
+    input_file  = os.path.join(ANTEADATADIR, 'full_body_pet.h5')
     output_file = os.path.join(config_tmpdir, 'test_run_script')
     rpos_file   = os.path.join(ANTEADATADIR, 'r_table_full_body.h5')
 
