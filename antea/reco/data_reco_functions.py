7686294306a09dfd559f153783d1404c61ae764f
Carmen Romo <carmenromo@Carmens-MacBook-Pro.local>
2021-11-24 11:21:27 +0100

    Add function to compute the charge percentage at corona

--- antea/reco/data_reco_functions.py
+++ antea/reco/data_reco_functions.py
@@ -58,6 +58,9 @@ def select_evts_with_max_charge_at_center(df: pd.DataFrame,
 int_area = [22, 23, 24, 25, 26, 27, 32, 33, 34, 35, 36, 37, 42, 43, 44, 45, 46, 47,
             52, 53, 54, 55, 56, 57, 62, 63, 64, 65, 66, 67, 72, 73, 74, 75, 76, 77]
 
+corona   = [11, 12, 13, 14, 15, 16, 17, 18, 21, 28, 31, 38, 41, 48,
+            51, 58, 61, 68, 71, 78, 81, 82, 83, 84, 85, 86, 87, 88]
+
 def is_event_contained_in_det_plane(df: pd.DataFrame) -> bool:
     """
     Returns True if all the sensors of the event are located within
@@ -78,3 +81,14 @@ def select_contained_evts_in_det_plane(df: pd.DataFrame) -> pd.DataFrame:
     """
     df_cov_evts = df.groupby(['evt_number', 'cluster']).filter(is_event_contained_in_det_plane)
     return df_cov_evts
+
+
+def compute_charge_percentage_in_corona(df: pd.DataFrame,
+                                        variable: str = 'efine_corrected') -> pd.Series:
+    """
+    Computes the percentage of charge detected in the external corona of the detection
+    plane with respect to the total charge of the that plane.
+    """
+    tot_ch_d = df[df.tofpet_id==0].groupby(['evt_number', 'cluster'])[variable].sum()
+    cor_ch   = df[df.sensor_id.isin(corona)].groupby(['evt_number', 'cluster'])[variable].sum()
+    return (cor_ch/tot_ch_d).fillna(0)*100
