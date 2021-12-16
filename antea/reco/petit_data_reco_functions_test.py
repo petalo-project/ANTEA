e576b4ae3759525b941b2fad3a284dc0afe3eb8f
Carmen Romo <carmenromo@lapromo.ific.uv.es>
2021-12-16 20:15:48 +0100

    Rename data test file and fix doc

--- antea/reco/petit_data_reco_functions_test.py
+++ antea/reco/petit_data_reco_functions_test.py
@@ -11,7 +11,7 @@ def test_compute_coincidences(ANTEADATADIR):
     """
     Checks that both planes of sensors have detected charge.
     """
-    PATH_IN  = os.path.join(ANTEADATADIR, 'data_petbox_test.h5')
+    PATH_IN  = os.path.join(ANTEADATADIR, 'petit_data_test.h5')
     df       = pd.read_hdf(PATH_IN, '/data_0')
     df_coinc = drf.compute_coincidences(df)
     sns      = df_coinc.groupby(['evt_number', 'cluster']).sensor_id.unique()
@@ -31,7 +31,7 @@ def test_select_evts_with_max_charge_at_center(ANTEADATADIR, det_plane, variable
     Checks that the max charge (in terms of the desired variable) is at center
     of the chosen plane.
     """
-    PATH_IN = os.path.join(ANTEADATADIR, 'data_petbox_test.h5')
+    PATH_IN = os.path.join(ANTEADATADIR, 'petit_data_test.h5')
     df      = pd.read_hdf(PATH_IN, '/data_0')
     df['intg_w_ToT'] = df['t2'] - df['t1']
 
@@ -60,7 +60,7 @@ def test_contained_evts_in_det_plane_and_compute_percentage_in_corona(ANTEADATAD
     Checks whether the event is fully contained in the detection plane and
     checks that the percentage of charge in the external corona is correct.
     """
-    PATH_IN  = os.path.join(ANTEADATADIR, 'data_petbox_test.h5')
+    PATH_IN  = os.path.join(ANTEADATADIR, 'petit_data_test.h5')
     df       = pd.read_hdf(PATH_IN, '/data_0')
     df_cov   = drf.select_contained_evts_in_det_plane(df)
     assert len(np.intersect1d(df_cov.sensor_id.unique(), drf.corona))==0
--- antea/scripts/process_data_petit_test.py
+++ antea/scripts/process_data_petit_test.py
@@ -5,10 +5,10 @@ from . process_data_petit import process_data_petit
 
 def test_process_data_petit(ANTEADATADIR, config_tmpdir):
     """
-    Checks that the function to extract data from data runs works.
+    Checks that the function to process petit data works.
     """
-    input_file  = os.path.join(ANTEADATADIR,  'data_petbox_test.h5')
-    output_file = os.path.join(config_tmpdir, 'test_data_extract_charge')
+    input_file  = os.path.join(ANTEADATADIR,  'petit_data_test.h5')
+    output_file = os.path.join(config_tmpdir, 'test_petit_data_process')
 
     try:
         process_data_petit(input_file, output_file)
