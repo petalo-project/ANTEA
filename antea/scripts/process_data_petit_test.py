009addeb9478ca267f5003310e1262307aa1a5b4
Carmen Romo <carmenromo@Carmens-MBP.home>
2021-12-17 12:21:10 +0100

    Add `.h5` to the output file

--- antea/scripts/process_data_petit_test.py
+++ antea/scripts/process_data_petit_test.py
@@ -8,7 +8,7 @@ def test_process_data_petit(ANTEADATADIR, config_tmpdir):
     Checks that the function to process petit data works.
     """
     input_file  = os.path.join(ANTEADATADIR,  'petit_data_test.h5')
-    output_file = os.path.join(config_tmpdir, 'test_petit_data_process')
+    output_file = os.path.join(config_tmpdir, 'test_petit_data_process.h5')
 
     try:
         process_data_petit(input_file, output_file)
