28fb7a257ed0f13f77c85949dfb699d041fa9eae
Nerea Salor <nereasalor@gmail.com>
2022-03-09 09:59:16 +0100

    Add function to calculate limits of tcoarse cycles

--- antea/preproc/tdc_corrections.py
+++ antea/preproc/tdc_corrections.py
@@ -6,3 +6,15 @@ def correct_tfine_wrap_around(df):
     Corrects the tfine values
     '''
     df['tfine'] = (df['tfine'] + 14) % 1024
+
+
+def compute_tcoarse_wrap_arounds(df):
+    '''
+    It returns the data position where tcoarse finishes a cycle, and the first
+    and last data position
+    '''
+    limits = df[df.tcoarse_diff < -30000].index #tcoarse cycle change
+    first  = df.index[0]
+    last   = df.index[-1]
+    limits = np.concatenate([np.array([first]), limits.values, np.array([last])])
+    return limits
--- antea/preproc/tdc_corrections_test.py
+++ antea/preproc/tdc_corrections_test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy  as np
 
 from antea.preproc.tdc_corrections import correct_tfine_wrap_around
+from antea.preproc.tdc_corrections import compute_tcoarse_wrap_arounds
 
 def test_correct_tfine_wrap_around():
     ''' Check the tfine correction '''
@@ -11,3 +12,16 @@ def test_correct_tfine_wrap_around():
     correct_tfine_wrap_around(df)
 
     np.testing.assert_array_equal(df['tfine'].values, expected_result)
+
+
+def test_compute_tcoarse_wrap_arounds():
+    ''' Check the limits of a new tcoarse cycle '''
+    df = pd.DataFrame({'tcoarse': [1500, 15000, 20000, 30500, 32000, 40000, 6000,
+                                   6050, 10000, 50000, 1000, 1100, 3000, 10000]})
+    df['tcoarse_diff'] = df.tcoarse.diff()
+    df = df.fillna(0)
+
+    limits          = compute_tcoarse_wrap_arounds(df)
+    expected_result = np.array([0, 6, 10, 13])
+
+    np.testing.assert_array_equal(limits, expected_result)
