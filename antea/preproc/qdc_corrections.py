e299ba2b59c4592a4ed81f2b74324da5721ef9ba
Nerea Salor <nereasalor@gmail.com>
2022-03-10 10:07:39 +0100

    Add function to compute qdc calibration

--- antea/preproc/qdc_corrections.py
+++ antea/preproc/qdc_corrections.py
@@ -17,3 +17,26 @@ def apply_qdc_autocorrection(df, df_qdc):
     '''
     df = df.merge(df_qdc, on=['tofpet_id', 'channel_id', 'tac_id', 'intg_w'])
     return (df['efine'] - df.offset)
+
+
+def compute_qdc_calibration_using_mode(df_tpulse, max_intg_w = 291):
+    '''
+    It sorts data taking into account tofpet id, channel id and tac id and it
+    calculates the integration window size and the corrected efine
+    '''
+    compute_integration_window_size(df_tpulse)
+    correct_efine_wrap_around(df_tpulse)
+
+    df_calib = df_tpulse.groupby(['tofpet_id', 'channel_id', 'tac_id',
+                                  'intg_w'])['efine'].agg(lambda x: x.value_counts().index[0])
+    df_calib = df_calib.reset_index()
+
+    # Replace first entries (< 5) with 0 to avoid out of range errors
+    # when interpolating (really) small windows.
+    df_calib.loc[df_calib.intg_w < 5, 'intg_w'] = 0
+    df_calib.loc[df_calib.intg_w == 0, 'efine'] = 0
+    # Replace last entry (291) with 2000 to avoid out of range errors
+    # when interpolating (really) large windows.
+    df_calib.loc[df_calib.intg_w == max_intg_w, 'intg_w'] = 2000
+
+    return df_calib
--- antea/preproc/qdc_corrections_test.py
+++ antea/preproc/qdc_corrections_test.py
@@ -3,6 +3,7 @@ import numpy  as np
 
 from antea.preproc.qdc_corrections import correct_efine_wrap_around
 from antea.preproc.qdc_corrections import apply_qdc_autocorrection
+from antea.preproc.qdc_corrections import compute_qdc_calibration_using_mode
 
 
 def test_correct_efine_wrap_around():
@@ -36,3 +37,25 @@ def test_apply_qdc_autocorrection():
     expected_result = np.array([0, 45, 70, 80, 100, 230, 100, 300, 90, 100, 250, 40])
 
     np.testing.assert_array_equal(diff, expected_result)
+
+
+def test_compute_qdc_calibration_using_mode():
+    '''
+    Check the data ordering in function of tofpet_id and channel_id and
+    the addition of integration window and efine corrected.
+    '''
+    df_tpulse   = pd.DataFrame({'tofpet_id' : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                'channel_id': [14, 17, 15, 17, 14, 15, 15, 14, 17, 14],
+                                'tac_id'    : [0, 1, 2, 3, 0, 1, 2, 3, 0, 1],
+                                'efine'     : [50, 100, 125, 300, 200, 350, 225, 290, 360, 150],
+                                'ecoarse'   : [150, 582, 6, 700, 1000, 1024, 300, 250, 800, 525],
+                                'tcoarse'   : [100, 4387, 28675, 33400, 40800, 59000, 200, 1024,  3600, 40200]})
+
+    df_calib    = compute_qdc_calibration_using_mode(df_tpulse)
+    df_expected = pd.DataFrame({'tofpet_id' : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                'channel_id': [14, 14, 14, 14, 15, 15, 15, 17, 17, 17],
+                                'tac_id'    : [0, 0, 1, 3, 1, 2, 2, 0, 1, 3],
+                                'intg_w'    : [50, 136, 261, 250, 392, 0, 100, 272, 2000, 68],
+                                'efine'     : [64, 214, 164, 304, 364, 0, 239, 374, 114, 314]})
+
+    np.testing.assert_array_equal(df_calib, df_expected)
