1ee3510e28fdcc995b949565efefbba5b79ebe14
Carmen Romo <carmenromo@lapromo.ific.uv.es>
2022-04-01 17:42:45 +0200

    Update mctrue function according to the new sim

--- antea/reco/mctrue_functions.py
+++ antea/reco/mctrue_functions.py
@@ -13,7 +13,7 @@ def find_hits_of_given_particles(p_ids: Sequence[int], hits: pd.DataFrame) -> pd
     return hits[hits.particle_id.isin(p_ids)]
 
 
-def select_photoelectric(evt_parts: pd.DataFrame, evt_hits: pd.DataFrame) -> Tuple[bool, Sequence[Tuple[float, float, float]]]:
+def select_photoelectric(evt_parts: pd.DataFrame, evt_hits: pd.DataFrame, noncoll_gammas: bool = False) -> Tuple[bool, Sequence[Tuple[float, float, float]]]:
     """
     Select only the events where one or two photoelectric events occur, and nothing else.
     """
@@ -25,12 +25,16 @@ def select_photoelectric(evt_parts: pd.DataFrame, evt_hits: pd.DataFrame) -> Tup
     sel_hits   = find_hits_of_given_particles(ids, evt_hits)
     energies   = sel_hits.groupby(['particle_id'])[['energy']].sum()
     energies   = energies.reset_index()
-    energy_sel = energies[rf.greater_or_equal(energies.energy, 0.476443, allowed_error=1.e-6)]
 
-    sel_vol_name_e  = sel_vol_name[sel_vol_name.particle_id.isin(energy_sel.particle_id)]
+    if noncoll_gammas:
+        energy_sel = energies[(energies.energy >= 0.473) & (energies.energy <= 0.513)]
+        gammas     = evt_parts[evt_parts.particle_name == 'gamma']
+    else:
+        energy_sel = energies[rf.greater_or_equal(energies.energy, 0.476443, allowed_error=1.e-6)]
+        gammas     = evt_parts[evt_parts.primary == True]
 
-    primaries = evt_parts[evt_parts.primary == True]
-    sel_all   = sel_vol_name_e[sel_vol_name_e.mother_id.isin(primaries.particle_id.values)]
+    sel_vol_name_e = sel_vol_name  [sel_vol_name  .particle_id.isin(energy_sel.particle_id)]
+    sel_all        = sel_vol_name_e[sel_vol_name_e.mother_id  .isin(gammas.particle_id.values)]
     if len(sel_all) == 0:
         return (False, [])
 
@@ -44,8 +48,9 @@ def select_photoelectric(evt_parts: pd.DataFrame, evt_hits: pd.DataFrame) -> Tup
         hit_positions = np.array([df.x.values, df.y.values, df.z.values]).transpose()
         true_pos.append(np.average(hit_positions, axis=0, weights=df.energy))
 
-    ### Reject events where the two gammas have interacted in the same hemisphere.
-    if (len(true_pos) == 1) & (evt_hits.energy.sum() > 0.511):
-       return (False, [])
+    if not noncoll_gammas:
+        ### Reject events where the two gammas have interacted in the same hemisphere.
+        if (len(true_pos) == 1) & (evt_hits.energy.sum() > 0.511):
+           return (False, [])
 
     return (True, true_pos)
