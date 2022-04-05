65dcb14fe5b0b3074f39bee2f0a29d3c2b1f5953
Nerea Salor <nereasalor@gmail.com>
2022-04-05 16:49:36 +0200

    Add apply qdc autocorrection function

--- antea/preproc/qdc_corrections.py
+++ antea/preproc/qdc_corrections.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import numpy  as np
 
+from antea.preproc.tdc_corrections import compute_integration_window_size
+
 def correct_efine_wrap_around(df):
     '''
     Corrects the efine value
     '''
     df['efine'] = (df['efine'] + 14) % 1024
+
+
+def apply_qdc_autocorrection(df, df_qdc):
+    '''
+    Corrects the efine value subtracting the offset value obtained with
+    the calibration in df_qdc
+    '''
+    df = df.merge(df_qdc, on=['tofpet_id', 'channel_id', 'tac_id', 'intg_w'])
+    return (df['efine'] - df.offset)
--- antea/preproc/qdc_corrections_test.py
+++ antea/preproc/qdc_corrections_test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy  as np
 
 from antea.preproc.qdc_corrections import correct_efine_wrap_around
+from antea.preproc.qdc_corrections import apply_qdc_autocorrection
 
 
 def test_correct_efine_wrap_around():
@@ -12,3 +13,26 @@ def test_correct_efine_wrap_around():
     correct_efine_wrap_around(df)
 
     np.testing.assert_array_equal(df['efine'].values, expected_result)
+
+
+def test_apply_qdc_autocorrection():
+    '''
+    Check the result of the efine  correction due to the offset
+    '''
+    df     = pd.DataFrame({'tofpet_id' : [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                           'channel_id': [4, 10, 13, 25, 29, 36, 42, 47, 58, 63, 42, 58],
+                           'tac_id'    : [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 0],
+                           'intg_w'    : [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 600, 800],
+                           'efine'     : [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]})
+
+
+    df_qdc = pd.DataFrame({'tofpet_id' : [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+                           'channel_id': [4,10, 13, 25, 29, 36, 42, 47, 58, 63 ],
+                           'tac_id'    : [0, 1, 2, 3, 0, 1, 2, 3, 0, 1],
+                           'intg_w'    : [0, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+                           'offset'    : [0, 5, 30, 70, 100, 20, 200, 260, 300, 410]})
+
+    diff            = apply_qdc_autocorrection(df, df_qdc).values
+    expected_result = np.array([0, 45, 70, 80, 100, 230, 100, 300, 90, 100, 250, 40])
+
+    np.testing.assert_array_equal(diff, expected_result)
