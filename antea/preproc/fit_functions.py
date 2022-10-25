e9a2a35963c011d4bab4f626e2a0e6b45d342a88
Nerea Salor <nereasalor@gmail.com>
2022-10-25 08:54:26 +0200

    Add skewnormal function

---
+++ antea/preproc/fit_functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from scipy.special    import erf
+
+
+def skewnormal_function(values, shape, location, scale, gain):
+    '''
+    Asymmetric gaussian function:
+    Shape tells us the symmetry, the sideways shift of the
+    center of the gaussine. Location is the center of the gaussine.
+    Scale is the parameter relationated with the gaussian sigma.
+    '''
+    t   = (values - location) / scale
+
+    pdf = np.exp(-t**2 / 2) / np.sqrt(2 * np.pi)
+    cdf = (1 + erf(shape * t / np.sqrt(2))) / 2
+
+    return 2 * gain / scale * pdf * cdf
