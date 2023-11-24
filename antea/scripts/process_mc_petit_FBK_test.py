f71819ba1554b51e8642f4e4dfb13d19f7959768
Nerea Salor <nereasalor@gmail.com>
2023-11-24 12:28:27 +0100

    Add test to check the MC FBK with saturation script works

---
+++ antea/scripts/process_mc_petit_FBK_test.py
@@ -0,0 +1,16 @@
+import os
+
+from . process_mc_petit_FBK import process_mc_petit_FBK
+
+
+def test_process_mc_petit(ANTEADATADIR, config_tmpdir):
+    """
+    Checks that the function to process petit data with FBK SiPMs works.
+    """
+    input_file  = os.path.join(ANTEADATADIR,  'petit_mc_fbk_test.pet.h5')
+    output_file = os.path.join(config_tmpdir, 'test_petit_mc_fbk_process.h5')
+
+    try:
+        process_mc_petit_FBK(input_file, output_file)
+    except:
+        raise AssertionError('Function process_mc_petit_FBK has failed running.')
