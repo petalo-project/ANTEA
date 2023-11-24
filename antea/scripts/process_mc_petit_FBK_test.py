4df81e32608601ea86352a33b19a48af7df0ed53
Nerea Salor <nereasalor@gmail.com>
2023-11-24 13:08:39 +0100

    Add variable to MC FBK script test

--- antea/scripts/process_mc_petit_FBK_test.py
+++ antea/scripts/process_mc_petit_FBK_test.py
@@ -9,8 +9,9 @@ def test_process_mc_petit_FBK(ANTEADATADIR, config_tmpdir):
     """
     input_file  = os.path.join(ANTEADATADIR,  'petit_mc_fbk_test.pet.h5')
     output_file = os.path.join(config_tmpdir, 'test_petit_mc_fbk_process.h5')
+    recovery_time = 80
 
     try:
-        process_mc_petit_FBK(input_file, output_file)
+        process_mc_petit_FBK(input_file, output_file, recovery_time)
     except:
         raise AssertionError('Function process_mc_petit_FBK has failed running.')
